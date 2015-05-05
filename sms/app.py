d300a12167867349082b5d31dfc6bf3153c37c20
Frank Harris <fcharris@gmail.com>
2015-05-05 10:03:16 -0400

    Only return first name; resolves #16

--- sms/app.py
+++ sms/app.py
@@ -74,7 +74,8 @@ def sms():
                 u = new_user
 
                 # Welcome reply message
-                reply = 'Hi ' + message + ', welcome to Bowery SMS! We\'ll send you the workout every morning.\n\nText "Help" for a list of other stuff you can do.'
+                # Only return first name
+                reply = 'Hi ' + message.split(" ")[0] + ', welcome to Bowery SMS! We\'ll send you the workout every morning.\n\nText "Help" for a list of other stuff you can do.'
         else:
             # Lowercase the inbound message
             message = message.lower()
